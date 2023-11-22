1bdb64e578bf08a128f12d430a13b0f9d60e7c15
Jesus <delgadoperdomoje@uvm.edu.ve>
2023-11-22 17:38:13 -0400

    Ocultar Credenciales e instalacion del bcryp

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'robotdb'
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_DATABASE
 });
  
 connection.connect();
